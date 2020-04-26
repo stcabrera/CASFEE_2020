55a801104ac40accfe88275d8e6fdff716d30834
stcabrera <stephancabrera@gmail.com>
2020-04-26 18:41:11 +0200

    0.1_addButton

--- script/script.js
+++ script/script.js
@@ -1,2 +1,7 @@
-// Test test
-console.log('testtest');
+var button = document.getElementById("add");
+
+ button.addEventListener("click", createNote);
+
+ function createNote() {
+    console.log('add geklickt');
+ }
