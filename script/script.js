82678a3cf0edc0bf96abb61e00853badf5b1db41
stcabrera <stephancabrera@gmail.com>
2020-04-26 20:24:02 +0200

    getInput

--- script/script.js
+++ script/script.js
@@ -1,6 +1,23 @@
-var button = document.querySelector("#add");
+let button = document.querySelector('#add');
+let closeButton = document.querySelector('.closeForm');
+let saveButton = document.querySelector('#save');
+
 button.addEventListener("click", createNote);
+closeButton.addEventListener("click", closeForm);
+saveButton.addEventListener('click', getInput);
 
  function createNote() {
    document.querySelector('#modalForm').style.left = '0';
  }
+ function closeForm() {
+    document.querySelector('#modalForm').style.left = '-350px';
+  }
+
+  function getInput() {
+    let title = 'Title: ' + document.querySelector('#title').value;
+    let note = 'Note: ' + document.querySelector('#note').value;
+    let importance = 'Importance: ' + document.querySelector('#importance').value;
+    let date = 'Due date: ' + document.querySelector('#date').value;
+    console.log(title + ' ' + note + ' ' + importance + ' ' + date);
+  }
+
