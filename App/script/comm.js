404a4d76a9bb4e0b79f7d3ab5e31ea237a23c55a
stephan cabrera <stephancabrera@gmail.com>
2020-06-18 16:34:36 +0200

    get Data

--- App/script/comm.js
+++ App/script/comm.js
@@ -1,3 +1,25 @@
-fetch('http://localhost:3000/tasks')
-  .then(response => response.json())
-  .then(data => console.log(data));
+function getData() {
+  fetch('http://localhost:3000/tasks')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      var list = document.querySelector('#list');
+      var li = document.createElement('li');
+          li.className = "listItem";
+          li.innerHTML = data[i].title + '<br>' + data[i].note + '</br>' + data[i].importance + '<br>' + data[i].dueDate;
+         
+         list.appendChild(li);
+    }
+  })
+
+  .catch(function(error) {
+    console.error(error);
+  });
+
+  
+}
+getData();
+
