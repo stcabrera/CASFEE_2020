b29edf0294e82661afb0ed33a126becde2908110
stephan cabrera <stephancabrera@gmail.com>
2020-07-13 20:44:37 +0200

    filter 1st step

--- App/script/controller.js
+++ App/script/controller.js
@@ -1,16 +1,70 @@
-const server = 'http://localhost:3000/tasks/';
+let taskData = [];
+const buttonASC = document.querySelector('#sortASC');
+const buttonDESC = document.querySelector('#sortDESC');
+const container = document.querySelector('#container');
+const source = document.querySelector('#task-list-template').innerHTML;
+const template = Handlebars.compile(source);
+buttonASC.addEventListener('click', function() {
+    console.log('asc')
+    asc()
+})
+buttonDESC.addEventListener('click', function() {
+    console.log('desc')
+    desc()
+})
 
+
+
+function asc() {
+    function renderTasksASC() {
+        container.innerHTML = template(tasksSorted());
+    }
+
+    function compareTasks(s1, s2) {
+        return s2.title + s1.title;
+    }
+
+    function tasksSorted() {
+        return [...taskData].sort(compareTasks);
+    }
+    renderTasksASC()
+}
+
+function desc() {
+    function renderTasksASC() {
+        container.innerHTML = template(tasksSorted());
+    }
+
+    function compareTasks(s1, s2) {
+        return s2.title - s1.title;
+    }
+
+    function tasksSorted() {
+        return [...taskData].sort(compareTasks);
+    }
+    renderTasksASC()
+}
+
+
+
+const server = 'http://localhost:3000/tasks/';
 (function getData() {
     fetch(server)
         .then(function(response) {
-            return response.json();
+            return response.json()
         })
         .then(function(data) {
             const tasks = { tasks: data }
-            renderTasks(tasks)
+
+            taskData = tasks.tasks
+            console.log(taskData)
+            desc()
+
         });
+
 })();
 
+/*
 function renderTasks(tasks) {
     let source = document.querySelector('#task-list-template').innerHTML
     let template = Handlebars.compile(source);
@@ -18,29 +72,27 @@ function renderTasks(tasks) {
     let list = document.querySelector('#list')
     list.innerHTML = result
 }
-
+*/
 function pushData() {
     fetch(server, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "title": document.querySelector('#title').value,
-                "note": document.querySelector('#note').value,
-                "importance": document.querySelector('#importance').value,
-                "dueDate": document.querySelector('#date').value,
-                "created": document.querySelector('#today').value,
-                "done": 'undone'
-            })
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "title": document.querySelector('#title').value,
+            "note": document.querySelector('#note').value,
+            "importance": document.querySelector('#importance').value,
+            "dueDate": document.querySelector('#date').value,
+            "created": document.querySelector('#today').value,
+            "done": 'undone'
         })
-        .catch(error => console.log('error', error));
-    location.reload()
+    })
+
+
 }
 
 function deleteTask() {
     if (event.target.classList.contains('delete')) {
         const itemKey = event.target.parentElement.parentElement.parentElement.parentElement.id;
-        console.log(itemKey)
-
         fetch(server + itemKey, {
             method: 'DELETE'
         })
--- App/script/view.js
+++ App/script/view.js
@@ -1,40 +1,31 @@
-let list = document.querySelector('#list');
+let list = document.querySelector('#list')
 
 //Push Data to API
-const saveButton = document.querySelector('#save');
-saveButton.addEventListener('click', pushData);
+const saveButton = document.querySelector('#save')
+saveButton.addEventListener('click', pushData)
 saveButton.addEventListener("click", function() {
-    document.querySelector('#modalForm').style.top = '-100vh';
+    document.querySelector('#modalForm').style.top = '-100vh'
 });
-
 
 // Check Task
 list.addEventListener('click', event => {
-    const myHeaders = new Headers();
     if (event.target.classList.contains('check')) {
-        const itemKey = event.target.parentElement.parentElement.parentElement.id;
+        const itemKey = event.target.parentElement.parentElement.parentElement.id
         if (event.target.classList.contains('undone')) {
-            myHeaders.append("Content-Type", "application/json");
-            const requestOptions = {
+            fetch(server + itemKey, {
                 method: 'PATCH',
-                headers: myHeaders,
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ "done": 'done' }),
-                redirect: 'follow'
-            };
+            })
 
-            fetch(server + itemKey, requestOptions)
             location.reload()
 
         } else {
-            myHeaders.append("Content-Type", "application/json");
-            const requestOptions = {
+            fetch(server + itemKey, {
                 method: 'PATCH',
-                headers: myHeaders,
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ "done": 'undone' }),
-                redirect: 'follow'
-            };
-
-            fetch(server + itemKey, requestOptions)
+            })
             location.reload()
         }
     }
